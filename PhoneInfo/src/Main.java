6cec6d969b3fb3ceab5122fcab6de158d7c5e7ca
kdm-korea <kdm_korea@naver.com>
2017-10-12 15:41:57 +0900

    전화번호부 Insert, Delete, Select문 구현

---
+++ PhoneInfo/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Manager mg = new Manager();
+		while (true) {
+
+			switch (showMenu()) {
+			case 1:
+				mg.save();
+				continue;
+			case 2:
+				mg.search();
+				continue;
+			case 3:
+				mg.delete();
+				continue;
+			case 4:
+				break;
+			default:
+				System.out.println("잘못된 선택입니다.");
+				continue;
+			}
+			System.out.println("종료되었습니다");
+			break;
+		}
+
+	}
+
+	public static int showMenu() {
+		System.out.println("선택하세요..");
+		System.out.println("1.데이터 입력");
+		System.out.println("2.데이터 검색");
+		System.out.println("3.데이터 삭제");
+		System.out.println("4.프로그램 종료");
+		System.out.println("선택 : ");
+		int choose = sc.nextInt();
+		sc.nextLine();
+		return choose;
+	}
+}
---
+++ PhoneInfo/src/Manager.java
@@ -0,0 +1,57 @@
+public class Manager {
+	PhoneInfo[] array = new PhoneInfo[100];
+	int index = 0;
+
+	void save() {
+		array[index++] = readData();
+		array[index - 1].showInfo();
+	}
+
+	void search() {
+		System.out.println("데이터 검색을 시작합니다...");
+		System.out.println("name : ");
+		int val = matchName(Main.sc.nextLine());
+		if (val >= 0) {
+			array[val].showInfo();
+		}
+	}
+
+	void delete() {
+		System.out.println("데이터 삭제를 시작합니다...");
+		System.out.println("name : ");
+		int val = matchName(Main.sc.nextLine());
+		if (val >= 0) {
+			for (int tmp = val; tmp <= index; tmp++) {
+				array[tmp] = array[tmp + 1];
+			}
+			index--;
+			System.out.println("삭제되었습니다.");
+		}
+	}
+
+	PhoneInfo readData() {
+		System.out.println("이름 : ");
+		String name = Main.sc.nextLine();
+		System.out.println("핸드폰 번호 : ");
+		String phoneNumber = Main.sc.nextLine();
+		System.out.println("생일 : ");
+		String birthday = Main.sc.nextLine();
+		PhoneInfo info = new PhoneInfo(name, phoneNumber, birthday);
+		return info;
+	}
+
+	int matchName(String name) {
+		try {
+			for (int tmp = 0; tmp <= index; tmp++) {
+				if (name.equals(array[tmp].name)) {
+					return tmp;
+				}
+			}
+		} catch (Exception NullPointerException) {
+			System.out.println("찾지 못하였습니다..");
+			return -1;
+		}
+		System.out.println("찾지 못하였습니다..");
+		return -1;
+	}
+}
